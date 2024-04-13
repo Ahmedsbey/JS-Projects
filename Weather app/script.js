616ec1606836552601c3967751e490b3d2ffa867
Ahmedsbey <ahmedraz2533@gmail.com>
2024-04-13 18:27:24 +0100

    f

--- Weather app/script.js
+++ Weather app/script.js
@@ -4,10 +4,10 @@ let city = ``
 const input = document.querySelector('#cityName')
 const search = document.querySelector('#search')
 const cityname = document.querySelector('#city')
+const errorp = document.querySelector('#error')
 const temperature = document.querySelector('#temp')
 const wind = document.querySelector('#wind')
 const humidity = document.querySelector('#humidity')
-
 search.addEventListener('click', async(e)=>{
     e.preventDefault()
     city = input.value
@@ -15,12 +15,18 @@ search.addEventListener('click', async(e)=>{
 })
 async function run(){
    response = await fetch(`${api}${city}&appid=${apiKey}`)
-   let data = await response.json()
-   // console.log(data)
-   // console.log(input.value)
-   cityname.innerHTML = `${input.value.toUpperCase()}`
-   temperature.innerHTML = `${(data.main.temp.toFixed(0))}°C`
-   wind.innerHTML = `${data.wind.speed.toFixed(0)} KMPH`
-   humidity.innerHTML = `${data.main.humidity}%`
-
+   try {
+      if(!response.ok){
+         throw new Error('not found or server error, Please enter a valid city name')
+      }
+      let data = await response.json()
+      cityname.innerHTML = `${input.value.toUpperCase()}`
+      temperature.innerHTML = `${(data.main.temp.toFixed(0))}°C`
+      wind.innerHTML = `${data.wind.speed.toFixed(0)} KMPH`
+      humidity.innerHTML = `${data.main.humidity}%`
+   } catch (error) {
+      console.error(error.message)
+      errorp.innerHTML = `${input.value} ${error.message}`
+    
+   }
 }
